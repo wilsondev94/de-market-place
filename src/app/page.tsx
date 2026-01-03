6b3258c78eef80db71246950db0e99b821f4f45e
devwilson <ohioleomole@gmail.com>
2026-01-03 15:20:48 +0000

    landing page: implement desktop navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import MaxWidthWrapper from "@/components/shared/MaxWidthWrapper";
 import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
