b89270621b1de012987755bb6dbaaa861d90df9b
devwilson <ohioleomole@gmail.com>
2026-01-04 11:49:53 +0000

    landing page: implement the mobile nav

--- src/components/navs/Navbar.tsx
+++ src/components/navs/Navbar.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
-import { Icons } from "../Icons";
-import MaxWidthWrapper from "../shared/MaxWidthWrapper";
 import { buttonVariants } from "../ui/button";
+import MaxWidthWrapper from "../shared/MaxWidthWrapper";
+import { Icons } from "../Icons";
 import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Navbar = async () => {
   const user = null;
@@ -13,6 +14,8 @@ const Navbar = async () => {
         <MaxWidthWrapper>
           <div className="border-b border-gray-200">
             <div className="flex h-16 items-center">
+              <MobileNav />
+
               <div className="ml-4 flex lg:ml-0">
                 <Link href="/">
                   <Icons.logo className="h-10 w-10" />
