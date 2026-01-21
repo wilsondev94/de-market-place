889d64270d75ecbd2589de50f1df6ac3216d9abb
devwilson <ohioleomole@gmail.com>
2026-01-21 15:45:27 +0000

    implement user account menu for navbar

--- src/components/navs/Navbar.tsx
+++ src/components/navs/Navbar.tsx
@@ -5,9 +5,13 @@ import { Icons } from "../Icons";
 import NavItems from "./NavItems";
 import MobileNav from "./MobileNav";
 import Cart from "../Cart";
+import { getServerSideUser } from "@/lib/payload-utils";
+import { cookies } from "next/headers";
+import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async () => {
-  const user = null;
+  const nextCookies = cookies();
+  const { user } = await getServerSideUser(nextCookies);
 
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
@@ -59,7 +63,9 @@ const Navbar = async () => {
                     <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
                   ) : null}
 
-                  {user ? null : (
+                  {user ? (
+                    <UserAccountNav user={user} />
+                  ) : (
                     <div className="flex lg:ml-6">
                       <span
                         className="h-6 w-px bg-gray-200"
