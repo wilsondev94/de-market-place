0c59fe6d1afde2d687f56867c8fbc7196b0f5c8f
devwilson <ohioleomole@gmail.com>
2026-02-11 17:22:43 +0000

    add ProductFile and Order collections

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,6 +7,8 @@ import dotenv from "dotenv";
 import { Users } from "./collections/Users";
 import { Products } from "./collections/Products/Products";
 import { Media } from "./collections/Media";
+import { ProductFiles } from "./collections/ProductFiles";
+import { Orders } from "./collections/Order";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -14,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users, Products, Media],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: "/sell",
   },
