e74c987c13c03d366dac94cf6f1c0ad250398cf1
devwilson <ohioleomole@gmail.com>
2026-02-24 18:08:05 +0000

    thank-you page: render order items

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,9 +1,12 @@
-import { getPayloadClient } from "@/get-payload";
 import { getServerSideUser } from "@/lib/payload-utils";
-import { Order, Product } from "@/payload-types";
-import { cookies } from "next/headers";
 import Image from "next/image";
+import { cookies } from "next/headers";
+import { getPayloadClient } from "@/get-payload";
 import { notFound, redirect } from "next/navigation";
+import { Order, Product, ProductFile, User } from "@/payload-types";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
 
 interface PageProps {
   searchParams: {
@@ -42,9 +45,11 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
 
   const products = order.products as Product[];
 
-  const orderTotal = products.reduce((total, product) => {
+  const totalOrder = products.reduce((total, product) => {
     return total + product.price;
   }, 0);
+
+  const transactionFee = 1;
 
   return (
     <main className="relative lg:min-h-full">
@@ -85,6 +90,89 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
                 soon!
               </p>
             )}
+
+            <div className="mt-16 text-sm font-medium">
+              <div className="text-muted-foreground">Order nr.</div>
+              <div className="mt-2 text-gray-900">{order.id}</div>
+
+              <ul className="mt-6 divide-y divide-gray-200 border-t border-gray-200 text-sm font-medium text-muted-foreground">
+                {(order.products as Product[]).map((product) => {
+                  const label = PRODUCT_CATEGORIES.find(
+                    ({ value }) => value === product.category,
+                  )?.label;
+
+                  const downloadUrl = (product.product_files as ProductFile)
+                    .url as string;
+
+                  const { image } = product.images[0];
+
+                  return (
+                    <li key={product.id} className="flex space-x-6 py-6">
+                      <div className="relative h-24 w-24">
+                        {typeof image !== "string" && image.url ? (
+                          <Image
+                            fill
+                            src={image.url}
+                            alt={`${product.name} image`}
+                            className="flex-none rounded-md bg-gray-100 object-cover object-center"
+                          />
+                        ) : null}
+                      </div>
+
+                      <div className="flex-auto flex flex-col justify-between">
+                        <div className="space-y-1">
+                          <h3 className="text-gray-900">{product.name}</h3>
+
+                          <p className="my-1">Category: {label}</p>
+                        </div>
+
+                        {order._isPaid ? (
+                          <a
+                            href={downloadUrl}
+                            download={product.name}
+                            className="text-blue-600 hover:underline underline-offset-2"
+                          >
+                            Download asset
+                          </a>
+                        ) : null}
+                      </div>
+
+                      <p className="flex-none font-medium text-gray-900">
+                        {formatPrice(product.price)}
+                      </p>
+                    </li>
+                  );
+                })}
+              </ul>
+
+              <div className="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-muted-foreground">
+                <div className="flex justify-between">
+                  <p>Subtotal</p>
+                  <p className="text-gray-900">{formatPrice(totalOrder)}</p>
+                </div>
+
+                <div className="flex justify-between">
+                  <p>Transaction Fee</p>
+                  <p className="text-gray-900">{formatPrice(transactionFee)}</p>
+                </div>
+
+                <div className="flex items-center justify-between border-t border-gray-200 pt-6 text-gray-900">
+                  <p className="text-base">Total</p>
+                  <p className="text-base">
+                    {formatPrice(totalOrder + transactionFee)}
+                  </p>
+                </div>
+              </div>
+
+              <div className="mt-16 border-t border-gray-200 py-6 text-right">
+                <Link
+                  href="/products"
+                  className="text-sm font-medium text-blue-600 hover:text-blue-500"
+                >
+                  Continue shopping &rarr;
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
