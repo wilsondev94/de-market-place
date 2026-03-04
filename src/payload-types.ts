5fc92c339bdf44a22c7b41bbc0660fac291aedc0
devwilson <ohioleomole@gmail.com>
2026-03-04 18:09:23 +0000

    sync users to created products

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -13,8 +13,8 @@ export interface Config {
     media: Media;
     product_files: ProductFile;
     orders: Order;
-    "payload-preferences": PayloadPreference;
-    "payload-migrations": PayloadMigration;
+    'payload-preferences': PayloadPreference;
+    'payload-migrations': PayloadMigration;
   };
   globals: {};
 }
@@ -24,7 +24,9 @@ export interface Config {
  */
 export interface User {
   id: string;
-  role: "admin" | "user";
+  products?: (string | Product)[] | null;
+  product_files?: (string | ProductFile)[] | null;
+  role: 'admin' | 'user';
   updatedAt: string;
   createdAt: string;
   email: string;
@@ -48,9 +50,9 @@ export interface Product {
   name: string;
   description?: string | null;
   price: number;
-  category: "ui_kits" | "icons";
+  category: 'ui_kits' | 'icons';
   product_files: string | ProductFile;
-  approveForSale?: ("pending" | "approved" | "denied") | null;
+  approveForSale?: ('pending' | 'approved' | 'denied') | null;
   priceId?: string | null;
   stripeId?: string | null;
   images: {
@@ -141,7 +143,7 @@ export interface Order {
 export interface PayloadPreference {
   id: string;
   user: {
-    relationTo: "users";
+    relationTo: 'users';
     value: string | User;
   };
   key?: string | null;
@@ -169,6 +171,7 @@ export interface PayloadMigration {
   createdAt: string;
 }
 
-// declare module 'payload' {
-//   export interface GeneratedTypes extends Config {}
-// }
+
+declare module 'payload' {
+  export interface GeneratedTypes extends Config {}
+}
